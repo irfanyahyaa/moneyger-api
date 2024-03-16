54e6f0ef346ccbea32c64b2981e64c3ae0e40705
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-03-17 00:43:46 +0700

    initial commit

---
+++ src/main/java/com/irfan/moneyger/MoneygerApplication.java
@@ -0,0 +1,13 @@
+package com.irfan.moneyger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneygerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneygerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irfan/moneyger/MoneygerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irfan.moneyger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneygerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
